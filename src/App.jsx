d5dd35a5b24dc9899e2b5511e22ca901c2b994d1
Kubra Tokgozlu <kubra.tokgozlu@gmail.com>
2024-05-20 00:13:53 +0200

    first iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { useState } from "react";
 import "./App.css";
+import contacts from "./contacts.json";
 
 function App() {
+
+  const [contactsData, setContactsData] = useState(contacts.slice(0,5))
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            contactsData.map(contact => (
+              <tr key={contact.id}>
+                <td><img src={contact.pictureUrl} alt={contact.name} style={{width:150, height:200}}/></td> 
+                <td>{contact.name}</td>
+                <td>{contact.popularity}</td>
+              </tr>
+            )
+
+            )
+          }
+
+        </tbody>
+      </table>
     </div>
   );
 }
